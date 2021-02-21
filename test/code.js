1c388340106611cef27b5af6a1d9d778c9bde0e6
Yikuan Sun <yikuansun2015@gmail.com>
2021-02-21 13:40:48 -0500

    commented the code

--- test/code.js
+++ test/code.js
@@ -1,18 +1,23 @@
+// define unit type that will be used every time
 const global_unit_type = "px";
 
 greenimg = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAAXNSR0IB2cksfwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAANQTFRFALAAQ7+TYgAAAApJREFUeJxjYAAAAAIAAUivpHEAAAAASUVORK5CYII=";
 
+// create canvas
 mycanvas = new daize.displayCanvas(800, 500, global_unit_type);
 document.body.appendChild(mycanvas);
 
+// create player sprite and add to canvas
 mysprite = new daize.sprite(100, 100, 100, 100, 0, global_unit_type);
 mycanvas.addsprite(mysprite);
-mysprite.costume = "https://bit.ly/dumbguypng";
+mysprite.costume = "https://bit.ly/dumbguypng"; // set fill image
 
+// create floor and add to canvas
 floor = new daize.sprite(800, 100, 400, 450, 0, global_unit_type);
 mycanvas.addsprite(floor);
-floor.costume = greenimg;
+floor.costume = greenimg; // set green fill
 
+// record key presses
 keysdown = {};
 document.body.addEventListener("keydown", function(e) {
     if (!e.repeat) {
@@ -23,6 +28,7 @@ document.body.addEventListener("keyup", function(e) {
     keysdown[e.key] = false;
 });
 
+// define variable for y velocity
 velocity_up = 0;
 
 function gameLoop() {
@@ -32,18 +38,22 @@ function gameLoop() {
     if (keysdown.ArrowLeft) {
         mysprite.x -= 5;
     }
+
+    // gravity
     if (!mysprite.boxcollision(floor)) {
         velocity_up -= 1;
     }
     else {
+        // move sprite up until out of floor
         while (mysprite.boxcollision(floor)) {
             velocity_up = 0;
             mysprite.y -= 1;
         }
-        mysprite.y += 1;
+        mysprite.y += 1; // push sprite back to level
+        //jump
         if (keysdown.x) {
             velocity_up = 10;
-            keysdown.x = false;
+            keysdown.x = false; // prevent hold down
         }
     }
     mysprite.y -= velocity_up;
