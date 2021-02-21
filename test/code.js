723005a640294f65aa22efdf5508fa75564ad5e2
Yikuan Sun <yikuansun2015@gmail.com>
2021-02-21 12:57:14 -0500

    Update code.js

--- test/code.js
+++ test/code.js
@@ -1,6 +1,8 @@
-mycanvas = new daize.displayCanvas(800, 500, "px");
+const global_unit_type = "px";
+
+mycanvas = new daize.displayCanvas(800, 500, global_unit_type);
 document.body.appendChild(mycanvas);
 
-mysprite = new daize.sprite(100, 100, 100, 100, 0, "px");
+mysprite = new daize.sprite(100, 100, 100, 100, 0, global_unit_type);
 mycanvas.addsprite(mysprite);
 mysprite.costume = "https://bit.ly/dumbguypng";
